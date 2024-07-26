b346a468478a45520e4036332ecb67ba192398ca
Tsvetomir Shterev <cvetomirshterev@students.softuni.bg>
2024-07-26 14:28:06 +0300

    removed redundant services

--- TimeLogs.API/Infrastructure/ServiceSettings.cs
+++ TimeLogs.API/Infrastructure/ServiceSettings.cs
@@ -1,12 +1,8 @@
-﻿using AutoMapper;
-using TimeLogs.DB.Commands.Projects;
+﻿using TimeLogs.DB.Commands.Projects;
 using TimeLogs.DB.Commands.Users;
 using TimeLogs.DB.Queries.TimeLogs;
 using TimeLogs.DB.Queries.Users;
-using TimeLogs.Services.Profiles;
-using TimeLogs.Services.Services.Projects;
 using TimeLogs.Services.Services.TimeLogs;
-using TimeLogs.Services.Services.Users;
 
 namespace TimeLogs.API.Infrastructure;
 
@@ -14,23 +10,12 @@ public static class ServiceSettings
 {
     public static void BuildServices(this WebApplicationBuilder builder)
     {
-        builder.Services.AddScoped<IUserService, UserService>();
         builder.Services.AddScoped<IUserCommands, UserCommands>();
         builder.Services.AddScoped<IUserQueries, UserQueries>();
 
-        builder.Services.AddScoped<IProjectService, ProjectService>();
         builder.Services.AddScoped<IProjectCommands, ProjectCommands>();
 
         builder.Services.AddScoped<ITimeLogService, TimeLogService>();
         builder.Services.AddScoped<ITimeLogQueries, TimeLogQueries>();
-
-        var mapperConfig = new MapperConfiguration(config =>
-        {
-            config.AddProfile(new ProjectProfile());
-            config.AddProfile(new UserProfile());
-        });
-
-        var mapper = mapperConfig.CreateMapper();
-        builder.Services.AddSingleton(mapper);
     }
 }
--- TimeLogs.Services/Services/Projects/IProjectService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TimeLogs.DB.Entities;
-using TimeLogs.Services.Dto.Projects;
-
-namespace TimeLogs.Services.Services.Projects;
-
-public interface IProjectService
-{
-    Project CreateProject (CreateProjectModel createProjectModel);    
-}
